2d613114681ce257373c9ecaf15483a9b54ec6df
bella-le <le.bella98@gmail.com>
2020-01-15 15:12:09 -0800

    added js, removed weird copy paste artifacts

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,10 @@ function initializePage() {
     $("a.thumbnail").click(projectClick);
 }
 
-function projectClick(e) { 
-    // prevent the page from reloading      
+function projectClick(e) {
+    // prevent the page from reloading
     e.preventDefault();
-    // In an event handler, $(this) refers to      
-    // the object that triggered the event      
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event   
     $(this).css("background-color", "#7fff00");
 }
